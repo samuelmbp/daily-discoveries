c6519e56b669faaec6f9bf1dde13267d7b7993ef
Samuel Raducan <samuel.thembp@gmail.com>
2023-09-18 15:36:32 +0100

    feat: render the list of button categories

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,5 +1,26 @@
+import { CATEGORIES } from "../utils/constants";
+
 const CategoryFilter = () => {
-  return <aside>CategoryFilter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+
+        {CATEGORIES.map((category) => (
+          <li key={category.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: category.color }}
+            >
+              {category.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 };
 
 export default CategoryFilter;
