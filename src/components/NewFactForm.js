c61997da7c71eec6541c099bb8a4f83c3a883b4e
Samuel Raducan <samuel.thembp@gmail.com>
2023-09-19 14:40:47 +0100

    feat: built the fact form

--- src/components/NewFactForm.js
+++ src/components/NewFactForm.js
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import { CATEGORIES } from "../utils/constants";
+
 const NewFactForm = () => {
-  return <form className="fact-form">Fact Form.</form>;
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const textLength = text.length;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(text, source, category);
+  };
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        type="text"
+        placeholder="Trustworthy source..."
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Choose category:</option>
+        {CATEGORIES.map((category) => (
+          <option key={category.name} value={category.name}>
+            {category.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
 };
 
 export default NewFactForm;
